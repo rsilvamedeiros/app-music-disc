789a055b7c1d3585bb4b878bfbf886e55b0af8b2
Renan Medeiros <renanmedeiros79@gmail.com>
2024-08-01 19:23:42 -0300

    Criando o model de usuários **backend/models/User.js

---
+++ backend/models/User.js
@@ -0,0 +1,14 @@
+// backend/models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["Admin", "Secretaria", "Publicador", "Cliente"],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
