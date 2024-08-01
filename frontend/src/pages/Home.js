683854fac9357379d21b004d2207b28d50f72781
Renan Medeiros <renanmedeiros79@gmail.com>
2024-08-01 19:37:15 -0300

    Criando as páginas do frontend ***frontend/src/pages/Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,17 @@
+// frontend/src/pages/Home.js
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Bem-vindo ao Sistema de Discografia</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
