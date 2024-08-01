87db92c6e7e40ff795bb46768051c2db45fbd272
Renan Medeiros <renanmedeiros79@gmail.com>
2024-08-01 19:40:30 -0300

    Criando as páginas do frontend ***frontend/src/pages/AlbumList.js

---
+++ frontend/src/pages/AlbumList.js
@@ -0,0 +1,35 @@
+// frontend/src/pages/AlbumList.js
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import axios from "axios";
+
+const AlbumList = () => {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/albums").then((response) => {
+      setAlbums(response.data);
+    });
+  }, []);
+
+  return (
+    <Container>
+      <Row>
+        {albums.map((album) => (
+          <Col key={album._id} md={4}>
+            <Card>
+              <Card.Img variant="top" src={album.cover} />
+              <Card.Body>
+                <Card.Title>{album.title}</Card.Title>
+                <Card.Text>{album.genre}</Card.Text>
+                <Card.Text>{album.releaseYear}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default AlbumList;
